fef6c263af85c3ac90b6a96260e8c3fa7c2ad309
msheffield <msheffield@adaptivebiotech.com>
2019-11-09 10:38:12 -0800

    added product sales column to products table

--- bamazon_seed.sql
+++ bamazon_seed.sql
@@ -10,9 +10,18 @@ CREATE TABLE products (
   department_name VARCHAR(100) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
   stock_quantity INTEGER(10) NOT NULL,
+  product_sales INTEGER(10) NULL,
 
   PRIMARY KEY (item_id)
 );
+
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    over_head_costs INT(10) NOT NULL,
+
+    PRIMARY KEY (department_id)
+)
 
 USE bamazon;
 
